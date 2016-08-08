396dea47363883331aadb0a5243672f7e68c430e
RK3833 <wkc1wnr2@gmail.com>
2016-08-08 02:47:07 +0000

    add feature/static

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+	def home
+		
+	end
+
+	def step
+		
+	end
+
+	def about
+		
+	end
+
+	def contents
+		
+	end
+
+	def contact
+		
+	end
+
+	def vision
+		
+	end
+
+	def members
+		
+	end
+end
