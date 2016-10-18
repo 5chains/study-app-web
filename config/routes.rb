54297fbbefe20b07d499eb57da9ae5fc25a9a6c9
higold2525 <takahirokaneko0605@gmail.com>
2016-10-18 11:17:50 +0000

    toppage4

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
    get 'inquiry' => 'inquiry#index'
    post 'inquiry/confirm' => 'inquiry#confirm'
    post 'inquiry/thanks' => 'inquiry#thanks'
+   get 'vision' => 'static_pages#vision'
 end
